3593acc63dbcad764d07970c021e4454c54a4b29
tylerholmes <tyler.gholmes@protonmail.com>
2022-02-23 22:57:07 -0500

    Echo Server

--- server.py
+++ server.py
@@ -1,5 +1,18 @@
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-port = 
+def main():
+    host = "localhost"
+    port = 5000
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((host,port))
+    while True:
+        sock.listen()
+        conn,addr = sock.accept()
+        message = conn.recv(1024)
+        if message == "end":
+            conn.sendall("dne")
+            conn.close()
+            return False
+        else:
+            conn.sendall(message[::-1])
+        
